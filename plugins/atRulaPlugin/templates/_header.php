487d289962d4b23f54b7b780a087ab91d85dd85e
MJ Suhonos <mj@suhonos.ca>
2015-08-07 13:48:40 -0400

    Header template updates

--- plugins/atRulaPlugin/templates/_header.php
+++ plugins/atRulaPlugin/templates/_header.php
@@ -8,50 +8,42 @@
 
 <header id="top-bar">
 
-  <?php if (sfConfig::get('app_toggleLogo')): ?>
-    <?php echo link_to(image_tag('/plugins/atRulaPlugin/images/logo.png'), '@homepage', array('id' => 'logo', 'rel' => 'home')) ?>
-  <?php endif; ?>
+	<div id="subhead">
 
-  <?php if (sfConfig::get('app_toggleTitle')): ?>
-    <h1 id="site-name">
-      <?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
-    </h1>
-  <?php endif; ?>
+	    <?php if (sfConfig::get('app_toggleLogo')): ?>
+	      <?php echo link_to(image_tag('/plugins/atRulaPlugin/images/logo.png'), '@homepage', array('id' => 'logo', 'rel' => 'home')) ?>
+	    <?php endif; ?>
 
-  <nav>
+	    <?php if (sfConfig::get('app_toggleTitle')): ?>
+	      <h1 id="site-name">
+	        <?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
+	      </h1>
+	    <?php endif; ?>
 
-    <?php echo get_component('menu', 'userMenu') ?>
+	    <nav>
 
-    <?php # echo get_component('menu', 'quickLinksMenu') ?>
+	      <?php echo get_component('menu', 'userMenu') ?>
 
-    <?php # echo get_component('menu', 'changeLanguageMenu') ?>
+	      <?php # echo get_component('menu', 'quickLinksMenu') ?>
 
-    <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+	      <?php # echo get_component('menu', 'changeLanguageMenu') ?>
 
-  </nav>
+	      <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
 
-  <div id="search-bar">
+	    </nav>
 
-    <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+	    <div id="search-bar">
 
-    <?php echo get_component('search', 'box') ?>
+	      <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
 
-  </div>
+	      <?php echo get_component('search', 'box') ?>
 
-  </section>
+	    </div>
 
-  <?php echo get_component_slot('header') ?>
+	    </section>
+
+	    <?php echo get_component_slot('header') ?>
+
+	</div>
 
 </header>
-
-<?php if (sfConfig::get('app_toggleDescription')): ?>
-  <div id="site-slogan">
-    <div class="container">
-      <div class="row">
-        <div class="span12">
-          <span><?php echo esc_specialchars(sfConfig::get('app_siteDescription')) ?></span>
-        </div>
-      </div>
-    </div>
-  </div>
-<?php endif; ?>
