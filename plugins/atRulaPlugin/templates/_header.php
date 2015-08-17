363f43eea7ee564c138e09ca84213aa3ed72ffab
Patrick Fung <patrick@makestuffdostuff.com>
2015-08-17 17:01:45 -0400

    fix broken tooltips

--- plugins/atRulaPlugin/templates/_header.php
+++ plugins/atRulaPlugin/templates/_header.php
@@ -18,51 +18,30 @@
 	header.rula {
 		height:50px; 
 	}
+
+	/*.rula #user-menu { margin-right: 0; }*/
 </style>
 
 <div class="header-wrap">
 	<header class="rula container">
 		<div class="row">
-			<div class="span2">
+			<div class="span6">
 				<?php if (sfConfig::get('app_toggleLogo')): ?>
 					<?php echo link_to(image_tag('/plugins/atRulaPlugin/images/logo.png'), '@homepage', array('id' => 'logo', 'rel' => 'home')) ?>
 				<?php endif; ?>
+
+				<?php if (sfConfig::get('app_toggleTitle')): ?>
+					<h1 id="site-name">
+						<?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
+					</h1>
+				<?php endif; ?>
 			</div>
-			<div class="span10">
-				<?php echo get_component('menu', 'userMenu') ?>
-				<?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+			<div class="span6">
+				<nav>
+					<?php echo get_component('menu', 'userMenu') ?>
+					<?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+				</nav>
 			</div>
 		</div>	
 	</header>
 </div>
-
-<?php /*
-<header id="top-bar">
-	<div id="subhead">
-		<?php if (sfConfig::get('app_toggleLogo')): ?>
-			<?php echo link_to(image_tag('/plugins/atRulaPlugin/images/logo.png'), '@homepage', array('id' => 'logo', 'rel' => 'home')) ?>
-		<?php endif; ?>
-
-		<?php if (sfConfig::get('app_toggleTitle')): ?>
-			<h1 id="site-name">
-				<?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
-			</h1>
-		<?php endif; ?>
-
-		<nav>
-			<?php echo get_component('menu', 'userMenu') ?>
-			<?php # echo get_component('menu', 'quickLinksMenu') ?>
-			<?php # echo get_component('menu', 'changeLanguageMenu') ?>
-			<?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-		</nav>
-
-<!-- 		<div id="search-bar">
-			<?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-			<?php echo get_component('search', 'box') ?>
-		</div> -->
-
-		<?php echo get_component_slot('header') ?>
-		
-	</div>
-</header>
-*/ ?>
