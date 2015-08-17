ecef5f61a2671aad836ad33e4de2cbea44f6f3c1
Patrick Fung <patrick@makestuffdostuff.com>
2015-08-17 17:16:47 -0400

    clean up main layout: moved browse/searchbar to header partial

--- plugins/atRulaPlugin/templates/_header.php
+++ plugins/atRulaPlugin/templates/_header.php
@@ -6,14 +6,92 @@
 	</div>
 <?php endif; ?>
 
+<!-- Hacky inline styles -->
 <style type="text/css">
+	.main {
+		margin-top: 15px;
+	}
+	.main #browse-menu {
+		position: relative;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		width: 100%;
+	}
+
+	.main .top-item {
+		margin: 0;
+	}
+
+	.main .top-dropdown, 
+	.main a.top-dropdown:link, 
+	.main a.top-dropdown:visited, 
+	.main a.top-dropdown:active {
+		padding: 0 0 0 12px;
+		line-height: 28px;
+		width: 100%;
+		box-sizing: border-box;
+	}
+
+	#search-form-wrapper {
+		position: relative;
+	}
+
+	#search-form-wrapper form {
+		margin-bottom: 0;
+	}
+
+	#search-form-wrapper input {
+		margin-bottom: 0;
+	}
+
+	#search-form-wrapper button {
+		display: block;
+		width: 30px;
+		height: 30px;
+		right: 0px;
+		position: absolute;
+		top: 0px;
+		background-color: #1c5793;
+		color: white;
+		margin-top: 0px;
+		font-size: 1.1em;
+		border: none;
+		-webkit-border-radius: 0 4px 4px 0;
+		-moz-border-radius: 0 4px 4px 0;
+		border-radius: 0 4px 4px 0;
+	}
+
+	#search-form-wrapper button:before {
+		font-family: FontAwesome;
+		content: "\f002";
+	}
+
+	.search-popover {
+		top: 30px;
+	}
+
+	#search-bar .search-popover {
+		top: 25px;
+	}
+
+	@media (max-width: 767px) {
+		#search-form-wrapper {
+			margin-top: 10px;
+		}
+	}
+
+	@media (max-width: 480px) {
+		#browse-menu > a {
+			text-indent: 0;
+		}
+	}
 	.header-wrap {
-    background-color: #e6e6e6;
-    background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#fff), color-stop(50%, #eee), to(#e4e4e4));
-    background-image: -webkit-linear-gradient(#fff, #eee 50%, #e4e4e4);
-    background-image: -moz-linear-gradient(top, #fff, #eee 50%, #e4e4e4);
-    background-image: linear-gradient(#fff, #eee 50%, #e4e4e4);
-    background-repeat: no-repeat;
+		background-color: #e6e6e6;
+		background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#fff), color-stop(50%, #eee), to(#e4e4e4));
+		background-image: -webkit-linear-gradient(#fff, #eee 50%, #e4e4e4);
+		background-image: -moz-linear-gradient(top, #fff, #eee 50%, #e4e4e4);
+		background-image: linear-gradient(#fff, #eee 50%, #e4e4e4);
+		background-repeat: no-repeat;
 	}
 	header.rula {
 		height:50px; 
@@ -58,4 +136,15 @@
 			</div>
 		</div>	
 	</header>
+</div>
+
+<div class="container">
+	<div class="row main">
+		<div class="span2 clearfix">
+			<?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+		</div>
+		<div class="span10 clearfix">
+			<?php echo get_component('search', 'box') ?>
+		</div>
+	</div>
 </div>
--- plugins/atRulaPlugin/templates/layout_rulaHome.php
+++ plugins/atRulaPlugin/templates/layout_rulaHome.php
@@ -14,86 +14,7 @@
     <?php endif; ?>
     <?php include_javascripts() ?>
 
-    <!-- Hacky inline styles -->
-    <style type="text/css">
-      .main {
-        margin-top: 15px;
-      }
-      .main #browse-menu {
-        position: relative;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        width: 100%;
-      }
 
-      .main .top-item {
-        margin: 0;
-      }
-
-      .main .top-dropdown, 
-      .main a.top-dropdown:link, 
-      .main a.top-dropdown:visited, 
-      .main a.top-dropdown:active {
-        padding: 0 0 0 12px;
-        line-height: 28px;
-        width: 100%;
-        box-sizing: border-box;
-      }
-
-      #search-form-wrapper {
-        position: relative;
-      }
-
-      #search-form-wrapper form {
-        margin-bottom: 0;
-      }
-
-      #search-form-wrapper input {
-        margin-bottom: 0;
-      }
-      
-      #search-form-wrapper button {
-        display: block;
-        width: 30px;
-        height: 30px;
-        right: 0px;
-        position: absolute;
-        top: 0px;
-        background-color: #1c5793;
-        color: white;
-        margin-top: 0px;
-        font-size: 1.1em;
-        border: none;
-        -webkit-border-radius: 0 4px 4px 0;
-        -moz-border-radius: 0 4px 4px 0;
-        border-radius: 0 4px 4px 0;
-      }
-
-      #search-form-wrapper button:before {
-        font-family: FontAwesome;
-        content: "\f002";
-      }
-
-      .search-popover {
-        top: 30px;
-      }
-
-      #search-bar .search-popover {
-        top: 25px;
-      }
-
-      @media (max-width: 767px) {
-        #search-form-wrapper {
-          margin-top: 10px;
-        }
-      }
-
-      @media (max-width: 480px) {
-        #browse-menu > a {
-          text-indent: 0;
-        }
-      }
-    </style>
   </head>
 
   <body class="yui-skin-sam <?php echo $sf_context->getModuleName() ?> <?php echo $sf_context->getActionName() ?>">
@@ -101,16 +22,6 @@
     <?php echo get_partial('header') ?>
 
     <div class="container">
-
-      <div class="row main">
-        <div class="span2 clearfix">
-          <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-        </div>
-        <div class="span10 clearfix">
-          <?php echo get_component('search', 'box') ?>
-        </div>
-      </div>
-
       <div class="row">
         <div class="span12">
           <?php if (!include_slot('content')) : ?>
